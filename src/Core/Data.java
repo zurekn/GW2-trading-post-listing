2f4794a3bc3c6040cb4e6383ef7c096e97068018
zurekn <zurek.nicolas.2.0@gail.com>
2015-05-11 20:15:04 +0200

    a

--- src/Core/Data.java
+++ src/Core/Data.java
@@ -1,6 +1,5 @@
 package Core;
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -19,17 +18,14 @@ import org.jsoup.*;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
-
-
-
 public class Data {
 
-	private static final String gw2url = "http://www.gw2spidy.com/api/v0.9/json/"; 
+	private static final String gw2url = "http://www.gw2spidy.com/api/v0.9/json/";
 	private static HashMap<Long, String> items = new HashMap<Long, String>();
-	
+
 	public static StorageHandler storageHandler;
-	
-	public static void initAPI(){		
+
+	public static void initAPI() {
 		try {
 			storageHandler = new StorageHandler();
 		} catch (IOException e) {
@@ -37,60 +33,64 @@ public class Data {
 			e.printStackTrace();
 		}
 	}
-	
-	public static void loadItem() throws RemoteException{
-		
-        
-	}
-	
-	private static String readUrl(String urlString) throws Exception {
-	    BufferedReader reader = null;
-	    try {
-	        URL url = new URL(urlString);
-	        reader = new BufferedReader(new InputStreamReader(url.openStream()));
-	        StringBuffer buffer = new StringBuffer();
-	        int read;
-	        char[] chars = new char[1024];
-	        while ((read = reader.read(chars)) != -1)
-	            buffer.append(chars, 0, read); 
 
-	        return buffer.toString();
-	    } finally {
-	        if (reader != null)
-	            reader.close();
-	    }
+	public static void loadItem() throws RemoteException {
+
 	}
-	
-	public static JSONArray getItem(String name){
-		String[] split = name.split(" ");
-		if(split.length > 1){
-			boolean first = true;
-			name = "";
-			for(String s : split){
-				if(!first) name += "%20";
-				name += s;
-				first = false;
-			}
+
+	private static String readUrl(String urlString) throws Exception {
+		BufferedReader reader = null;
+		try {
+			URL url = new URL(urlString);
+			reader = new BufferedReader(new InputStreamReader(url.openStream()));
+			StringBuffer buffer = new StringBuffer();
+			int read;
+			char[] chars = new char[1024];
+			while ((read = reader.read(chars)) != -1)
+				buffer.append(chars, 0, read);
+
+			return buffer.toString();
+		} finally {
+			if (reader != null)
+				reader.close();
 		}
-		
-		String url = gw2url+"item-search/"+name;
-		System.out.println("Connecting to "+url);
+	}
+
+	public static JSONArray getItem(String name) {
+		name = parseForURL(name);
+		String url = gw2url + "item-search/" + name;
+		System.out.println("Connecting to " + url);
 		JSONObject json = new JSONObject();
 		JSONParser parser = new JSONParser();
 		JSONArray array = null;
 		try {
 			json = (JSONObject) parser.parse(readUrl(url));
 			System.out.println("---------------Info sur le JSON--------------");
-			System.out.println("ToString : "+json.toJSONString());
-	
+			System.out.println("ToString : " + json.toJSONString());
+
 			array = (JSONArray) json.get("results");
 			Iterator it = array.iterator();
-			JSONObject obj;			
-			
+			JSONObject obj;
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return array;
+	}
+
+	public static String parseForURL(String s) {
+		String[] split = s.split(" ");
+		if (split.length > 1) {
+			boolean first = true;
+			s = "";
+			for (String s1 : split) {
+				if (!first)
+					s += "%20";
+				s += s1;
+				first = false;
+			}
+		}
+		return s;
 	}
 }
--- src/Core/Search.java
+++ src/Core/Search.java
@@ -1,24 +1,25 @@
 package Core;
 
-
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.rmi.RemoteException;
+import java.util.Iterator;
 
 import org.json.simple.JSONArray;
-
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class Search {
 
 	private String name;
-	
-	
-	public Search(){
+
+	public Search() {
 
 	}
-	
-	public void selectItem(String nane){
+
+	public void selectItem(String name) {
 		try {
 			Data.storageHandler.saveSearch(name, "2");
 		} catch (IOException e) {
@@ -26,10 +27,22 @@ public class Search {
 			e.printStackTrace();
 		}
 	}
-	
-	public void updateName(String name){
+
+	public void updateName(String name) {
 		this.name = name;
 		JSONArray result = Data.getItem(name);
-		
+		Iterator it = result.iterator();
+		JSONParser parser = new JSONParser();
+		JSONObject obj;
+		while (it.hasNext()) {
+			try {
+				obj = (JSONObject) parser.parse(it.next().toString());
+				System.out.println(obj.get("name") + ", " + obj.get("data_id"));
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
 	}
 }
